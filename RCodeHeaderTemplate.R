f515cacf782de06f7c138e287ca1403150ca9877
FelipeMonts <frm10@psu.edu>
2019-09-24 15:01:02 -0400

    Moving code to the repository

---
+++ RCodeHeaderTemplate.R
@@ -0,0 +1,36 @@
+##############################################################################################################
+# 
+# 
+# Program to run TAU DEM form R for Watershed delineation 
+# 
+#     http://hydrology.usu.edu/taudem/taudem5/
+# 
+#    http://hydrology.usu.edu/taudem/taudem5/TauDEMRScript.txt
+# 
+# 
+#  Felipe Montes 2019 09/24
+# 
+# 
+# 
+# 
+############################################################################################################### 
+
+
+#  Tell the program where the package libraries are  #####################
+
+.libPaths("C:/Felipe/SotwareANDCoding/R_Library/library")  ;
+
+
+#      set the working directory
+
+
+setwd('C:/Felipe/Students Projects/Stephanie/HalfmoonWatershed/MM_PHIM_inputs') ;   #  setwd(RevisedOutputs.dir)   ;
+
+
+# Install the packages that are needed #
+
+
+
+
+###  load the libraries that are neded -- need to filter some of these  ---  
+
